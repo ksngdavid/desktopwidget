1b7dd3e8b3f1ef3233f0caeece233a79ceb64b91
ksngdavid <ksngdavid@gmail.com>
2023-11-01 23:46:21 +0800

    update PORT

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/',(req,res)=>res.send('custom redirect eq'));
 
-app.listen(3000);
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));   // Calling our app to listen to a certain port
